4a8817451cc6021de8f85a90338c1ed6c9140eaa
Jerglekakan <MrListerTehSisterFister@gmail.com>
2016-01-03 22:58:43 -0500

    Finished batch tag manipulation script. Still haven't thuroughly tested

--- admin/tag_ops.php
+++ admin/tag_ops.php
@@ -1,66 +1,312 @@
 <?php
 	if(!defined('_IN_ADMIN_HEADER_'))
 		die;
-	if(!isset($_GET['action']) || $_GET['action'] == '' || $_GET['action'] == 'none')
+	if(!isset($_GET['action']) || $_GET['action'] == '')
 	{
 		include "tagOpsForm.html";
 		exit;
 	}
-
-	$debug = true;
-	$all_posts = true;
-	if(isset($_GET['images']) && $_GET['images'] != '')
+	else if($_GET['action'] == 'none')
 	{
-		if($debug)
-			echo "Image set provided.<br/>";
-		$all_posts = false;
-		$image_set = explode(" ", $db->real_escape_string($_GET['images']));
-		if($debug)
+		echo "<h3 style='background-color:#f4afaf'>Pick an action ya putz!</h3>";
+		include "tagOpsForm.html";
+		exit;
+	}
+	$debug = false;
+	$all_posts = false;
+	$query = "SELECT id FROM $post_table WHERE "; //Used to get image set to work on
+	$regex_fields = array("tags","title",
+			"source","parent",
+			"creation_date",
+			"owner","height",
+			"width","ext");
+	$image_set = array();
+	$display_result_set = false;
+	$tclass = new tag();
+
+
+	//Populate $image_set
+	if(isset($_GET['images']) && $_GET['images'] != '') //If the user provided a set
+	{
+		if($_GET['set_type'] === "id_list") //List of post IDs
 		{
-			echo "All Posts: $debug<br/>
-			Image IDs provided:<br/>";
-			print_r($image_set);
-			echo "<br/>";
+			$image_set = explode(" ", $db->real_escape_string($_GET['images']));
+			$tmp_arr = array();
+			foreach($image_set as $thing)
+			{
+				if(($ofst = strpos($thing, '-')) !== false)
+				{
+					$range = explode("-", $thing);
+					$start = (int) $range[0];
+					$end = (int) $range[1];
+					foreach(range($start, $end) as $num)
+						$tmp_arr[] = $num;
+				}
+				else
+				{
+					if(is_numeric($thing))
+						$tmp_arr[] = $thing;
+				}
+			}
+			$image_set = $tmp_arr;
+		}
+		else //Regular Expression
+		{
+			$col = $db->real_escape_string($_GET['regex_against']);
+			$regex = $db->real_escape_string($_GET['images']);
+			if(array_search($col, $regex_fields) !== false)
+				$query .= "$col REGEXP '$regex';";
+			else
+			{
+				echo "<h4>The \"$col\" field has not been enabled for regular expressions</h4><br/>";
+				include "tagOpsForm.html";
+				exit();
+			}
+			if($debug) echo "<strong>SQL Query</strong><br/>$query<br/>";
+			$display_result_set = isset($_GET['display_regex']);
+			if($result = $db->query($query))
+			{
+				if($debug)
+				{
+					echo "Rows in result set: ".$result->num_rows."<br/>";
+					echo "<strong>Result dump:</strong><br/>";
+					var_dump($result);
+					echo "<br/>";
+				}
+				$row = $result->fetch_assoc();
+				while(!is_null($row))
+				{
+					if($debug)
+					{
+						echo "Next row:<br/>";
+						var_dump($row);
+						echo "<br/><br/>";
+					}
+					$image_set[] = $row['id'];
+					$row = $result->fetch_assoc();
+				}
+				if($display_result_set)
+				{
+					echo "Your regex will result in operation on these posts<br/>".
+					print_r($image_set, true)."<br/><br/>".
+					"<a href=\"index.php?page=tag_ops\">Back to form page</a>";
+					exit();
+				}
+				$result->free();
+			}
+			else
+			{
+				echo "SQL query returned empty result<br/>";
+			}
+		}
+	}//END User given image set
+	else
+	{
+		if($_GET['action'] === "add")
+		{
+			$all_posts = true;
+		}
+		else if($_GET['action'] === "remove" || $_GET['action'] === "replace")
+		{
+			$image_set = array();
+			$field = $_GET['action'].'_tags';
+			$itags = explode(" ", $_GET[$field]);
+			foreach($itags as $cur_tag)
+			{
+				if($cur_tag === end($itags))
+					$query .= "tags LIKE '% ".$db->real_escape_string($cur_tag)." %';";
+				else
+					$query .= "tags LIKE '% ".$db->real_escape_string($cur_tag)." %' OR ";
+			}
+
+			if($debug) echo "<strong>SQL Query</strong><br/>$query<br/>";
+			if($result = $db->query($query))
+			{
+				if($debug)
+				{
+					echo "Rows in result set: ".$result->num_rows."<br/>";
+					echo "<strong>Result dump:</strong><br/>";
+					var_dump($result);
+					echo "<br/>";
+				}
+				$row = $result->fetch_assoc();
+				while(!is_null($row))
+				{
+					if($debug)
+					{
+						echo "Next row:<br/>";
+						var_dump($row);
+						echo "<br/><br/>";
+					}
+					$image_set[] = $row['id'];
+					$row = $result->fetch_assoc();
+				}
+				$result->free();
+			}
+			else
+			{
+				echo "SQL Query returned empty result set<br/>";
+			}
+		}
+	}// END $image_set population
+
+	//Stop if there's no images to work on
+	if(empty($image_set) && !$all_posts)
+	{
+		echo "<strong>No posts found!</strong> Double check your tags.<br/>";
+		exit;
+	}
+	//Output image set
+	if($debug)
+	{
+		if($all_posts)
+		{
+			echo "Will operate on all posts!<br/><br/>";
+		}
+		else
+		{
+			echo "Will operate on these posts:<br/>".print_r($image_set, true);
+			echo "<br/><br/>";
 		}
 	}
 
+
 	if($_GET['action'] == "add")
 	{
-		if($debug)
-			echo "<h4>Adding Tags</h4><br/>";
 		$newtags = explode(" ", $db->real_escape_string($_GET['add_tags']));
 		if($debug)
 		{
-			echo "Tags:";
+			echo "<h4>Adding the following tags:</h4><br/>";
 			print_r($newtags);
-			echo "<br/>";
+			echo "<br/><br/>";
+		}
+
+		foreach($newtags as $cur_tag)
+		{
+			//Build update query
+			$query = "UPDATE $post_table SET tags = CONCAT(tags, '$cur_tag ') WHERE tags NOT LIKE '%$cur_tag%'";
+			if($all_posts)
+			{
+				$query .= ";";
+			}
+			else
+			{
+				$query .= " AND (";
+				foreach($image_set as $cur_id)
+				{
+					if($cur_id === end($image_set))
+						$query .= "id = $cur_id);";
+					else
+						$query .= "id = $cur_id OR ";
+				}
+			}
+	
+			//Execute Query (if we're not in debug mode)
+			if($debug)
+			{
+				echo "<strong>Update Query:</strong>$query<br/><br/>";
+			}
+			else
+			{
+				$db->query($query);
+				$qt = $db->affected_rows;
+				print "\"$cur_tag\" tag added to ".$qt." posts<br/>";
+				$tclass->addindextag($cur_tag, $qt);
+			}
 		}
 	}
 	else if($_GET['action'] == "remove")
 	{
-		if($debug)
-			echo "<h4>Removing Tags</h4><br/>";
 		$losetags = explode(" ", $db->real_escape_string($_GET['remove_tags']));
 		if($debug)
 		{
-			echo "Tags:<br/>";
+			echo "<h4>Removing the following tags</h4>";
 			print_r($losetags);
-			echo "<br/>";
+			echo "<br/><br/>";
+		}
+
+		foreach($losetags as $cur_tag)
+		{
+			//Build update query
+			$query = "UPDATE $post_table SET tags = REPLACE(tags, '$cur_tag ', '') WHERE tags LIKE '% $cur_tag %'";
+			if($all_posts)
+			{
+				$query .= ";";
+			}
+			else
+			{
+				$query .= " AND (";
+				foreach($image_set as $cur_id)
+				{
+					if($cur_id === end($image_set))
+						$query .= "id = $cur_id);";
+					else
+						$query .= "id = $cur_id OR ";
+				}
+			}
+	
+			//Execute Query (if we're not in debug mode)
+			if($debug)
+			{
+				echo "<strong>Update Query:</strong>$query<br/><br/>";
+			}
+			else
+			{
+				$db->query($query);
+				$qt = $db->affected_rows;
+				print "\"$cur_tag\" tag removed from ".$qt." posts<br/>";
+				$tclass->deleteindextag($cur_tag, $qt);
+			}
 		}
 	}
 	else if($_GET['action'] == "replace")
 	{
-		if($debug)
-			echo "<h4>Replacing Tags</h4><br/>";
-		$losetags = explode(" ", $db->real_escape_string($_GET['replace_tags']));
-		$newtags = explode(" ", $db->real_escape_string($_GET['new_tags']));
+		$losetag = $db->real_escape_string($_GET['replace_tags']);
+		$newtag = $db->real_escape_string($_GET['new_tags']);
+		if(substr($losetag, " ") != FALSE || substr($newtag, " ") != FALSE)
+		{
+			echo "<strong>When replacing one tag with another, neither of the input fields can contain a space</strong><br/>";
+			exit;
+		}
 		if($debug)
 		{
-			echo "Replacing:<br/>";
-			print_r($losetags);
-			echo "<br/>With:<br/>";
-			print_r($newtags);
-			echo "<br/>";
+			echo "<strong>Replacing:</strong><br/>";
+			print_r($losetag);
+			echo "<br/><strong>With:</strong><br/>";
+			print_r($newtag);
+			echo "<br/><br/>";
+		}
+
+		//Build update query
+		$query = "UPDATE $post_table SET tags = REPLACE(tags, ' $losetag ', ' $newtag ') WHERE tags LIKE '% $losetag %' AND tags NOT LIKE '% $newtag %'";
+		if($all_posts)
+		{
+			$query .= ";";
+		}
+		else
+		{
+			$query .= " AND (";
+			foreach($image_set as $cur_id)
+			{
+				if($cur_id === end($image_set))
+					$query .= "id = $cur_id);";
+				else
+					$query .= "id = $cur_id OR ";
+			}
+		}
+
+		//Execute Query (if we're not in debug mode)
+		if($debug)
+		{
+			echo "<strong>Update Query:</strong>$query<br/><br/>";
+		}
+		else
+		{
+			$db->query($query);
+			$qt = $db->affected_rows;
+			print "\"$losetag\" tag replaced with \"$newtag\" in $qt posts<br/>";
+			$tclass->deleteindextag($losetag, $qt);
+			$tclass->addindextag($newtag, $qt);
 		}
 	}
 	else
@@ -68,5 +314,4 @@
 		include "tagOpsForm.html";
 		exit;
 	}
-	
 ?>
---
+++ classes/tag.class.php
@@ -0,0 +1,76 @@
+<?php
+	class tag
+	{
+		function __construct()
+		{}
+		
+		function addindextag($tag, $qt = 1)
+		{
+			if($qt <= 0)
+				return;
+			global $db, $tag_index_table;
+			$tag = $db->real_escape_string($tag);
+			if($tag != "")
+			{
+				$query = "SELECT * FROM $tag_index_table WHERE tag='$tag'";
+				$result = $db->query($query);
+				if($result->num_rows == 1)
+				{
+					$row = $result->fetch_assoc();
+					$query = "UPDATE $tag_index_table SET index_count='".($row['index_count'] + $qt)."' WHERE tag='$tag'";
+				}
+				else
+					$query = "INSERT INTO $tag_index_table(tag, index_count) VALUES('$tag', '$qt')";
+				$db->query($query);
+			}
+		}
+		
+		function deleteindextag($tag, $qt = 1)
+		{
+			global $db, $tag_index_table;
+			$tag = $db->real_escape_string($tag);
+			if($tag != "")
+			{
+				$query = "SELECT index_count FROM $tag_index_table WHERE tag='$tag'";
+				$result = $db->query($query);
+				$row = $result->fetch_assoc();
+				if($row['index_count'] - $qt > 0)
+					$query = "UPDATE $tag_index_table SET index_count='".($row['index_count'] - $qt)."' WHERE tag='$tag'";
+				else
+					$query = "DELETE FROM $tag_index_table WHERE tag='$tag'";
+				$db->query($query);
+			}
+		}
+		
+		function alias($tag)
+		{
+			global $db, $alias_table;
+			$tag = $db->real_escape_string($tag);
+			$query = "SELECT tag FROM $alias_table WHERE alias='$tag' AND status='accepted'";
+			$result = $db->query($query);
+			$row = $result->fetch_assoc();
+			if($row['tag'] != "" && $row['tag'] != NULL)
+				return $row['tag'];
+			return false;
+		}
+		
+		function filter_tags($tags, $current, $ttags)
+		{
+			if(substr_count($tags, $current) > 1)
+			{
+				$temp_array = array();
+				$key_array = array_keys($ttags, $current);
+				$count = count($key_array)-1;
+				for($i = 1; $i <= $count; $i++)
+					$ttags[$key_array[$i]] = '';
+				foreach($ttags as $current)
+				{
+					if($current != "" && $current != " ")
+						$temp_array[] = $current;
+				}
+				$ttags = $temp_array;
+			}
+			return $ttags;
+		}
+	}
+?>
