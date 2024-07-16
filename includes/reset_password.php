ba0938e828cabb5dfe96174171cb9f18fd7e59e3
Jerglekakan <mrlistertehsisterfister@gmail.com>
2024-07-16 00:15:09 -0400

    Add May 16 2024 upstream changes

--- includes/reset_password.php
+++ includes/reset_password.php
@@ -21,7 +21,7 @@
 			if($row['email'] != "" && $row['email'] != NULL && strpos($row['email'],"@") !== false && strpos($row['email'],".") !== false && strlen($row['email']) > 2)
 			{		
 				$misc = new misc();
-				$code = hash('sha256',rand(132,1004958327747882664857));
+				$code = hash('sha256',bin2hex(random_bytes(128)).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()).mt_rand(0,getrandmax()));
 				$link = $site_url."/index.php?page=reset_password&code=".$code."&id=".$row['id'];
 				$body = 'A password reset has been requested for your account.<br /><br /> If you didn\'t request this, please ignore this email.<br /><br />To reset you password, please click on this link: <a href="'.$link.'">'.$link.'</a>';
 				$misc->send_mail($row['email'],$email_recovery_subject,$body);
@@ -61,7 +61,7 @@
 			print "Invalid reset link.<br />";
 		}
 	}
-	if(isset($_POST['new_password']) && $_POST['new_password'] != "" && isset($_SESSION['tmp_id']) && $_SESSION['tmp_id'] != "" && is_numeric($_SESSION['tmp_id']) && isset($_SESSION['reset_code']) && $_SESSION['reset_code'] != "")
+	if(isset($_POST['new_password']) && $_POST['new_password'] != "" && isset($_SESSION['tmp_id']) && $_SESSION['tmp_id'] != "" && is_numeric($_SESSION['tmp_id'])  && isset($_SESSION['reset_code']) && trim($_SESSION['reset_code']) != "" && $_SESSION['reset_code'] != "null")
 	{
 		$code = $db->real_escape_string($_SESSION['reset_code']);
 		$id = $db->real_escape_string($_SESSION['tmp_id']);
@@ -72,7 +72,7 @@
 		if($result->num_rows() > 0)
 		{
 			$user->update_password($id,$pass);
-			$query = "UPDATE $user_table SET mail_reset_code='' WHERE id='$id' AND mail_reset_code='$code'";
+			$query = "UPDATE $user_table SET mail_reset_code = NULL WHERE id='$id' AND mail_reset_code='$code'";
 			$db->query($query);
 			unset($_SESSION['tmp_id']);
 			unset($_SESSION['reset_code']);
