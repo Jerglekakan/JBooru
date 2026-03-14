efcb1d8db78c62bd3e651287554b5f3571006894
Jerglekakan <mrlistertehsisterfister@gmail.com>
2026-03-14 03:12:21 -0400

    remove mb_trim since devuan has it in a package now

--- functions.global.php
+++ functions.global.php
@@ -35,34 +35,4 @@
 			$db->query($query) or print $db->error.'<br />';
 		}
 	}
-	
-	function mb_trim($string, $charlist='\\\\s', $ltrim=true, $rtrim=true)
-    {
-        $both_ends = $ltrim && $rtrim;
-
-        $char_class_inner = preg_replace(
-            array( '/[\^\-\]\\\]/S', '/\\\{4}/S' ),
-            array( '\\\\\\0', '\\' ),
-            $charlist
-        );
-
-        $work_horse = '[' . $char_class_inner . ']+';
-        $ltrim && $left_pattern = '^' . $work_horse;
-        $rtrim && $right_pattern = $work_horse . '$';
-
-        if($both_ends)
-        {
-            $pattern_middle = $left_pattern . '|' . $right_pattern;
-        }
-        elseif($ltrim)
-        {
-            $pattern_middle = $left_pattern;
-        }
-        else
-        {
-            $pattern_middle = $right_pattern;
-        }
-
-        return preg_replace("/$pattern_middle/usSD", '', $string);
-    } 
 ?>
